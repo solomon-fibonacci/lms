311993d1a7ea177772493dc72e9bd6390c142a97
solomon-fibonacci <omojolasolomon@gmail.com>
2016-05-17 17:17:43 +0100

    Updates to the config file

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-    $dbname="library"; //database Name
-    $dbhost="localhost"; //database Host
+    $dbname="library"; //database name
+    $dbhost="localhost"; //database host
     $dbuser="library"; //database user name
-    $dbpass="library"; //database Passwoed
+    $dbpass="library"; //database default password
     $max=7; //Max Day For Book Return
     $maxBooking=2; //Max number of books that can borrowed at once
     $maxExpiry=3; //Max day for reservation to expire
     $projectName="LMS"; //Project/Site Name
     $user="admin"; //Username For Admin
-    $password="admin";//Password Word For Admin
+    $password="admin";//Default Password Word For Admin
 ?>
